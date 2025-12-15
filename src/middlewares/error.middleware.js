ddde7a5739966b983d12a0d95f71d81a4f3a36f4
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-12-15 11:37:01 -0300

    feat: added error.middleware.js file

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,12 @@
+export default function errorMiddleware(err, req, res, next) {
+  const status = err.statusCode || 500;
+  const message = status === 500 ? "Internal server error!" : err.message;
+
+  if ((status = 500)) {
+    console.error("[ERROR]", err);
+  }
+
+  res.status(status).json({
+    error: message,
+  });
+}
