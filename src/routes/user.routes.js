c2ca4b566b5c911bfdf89165b187984e0058ef41
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-12-16 13:57:36 -0300

    feat: added user routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import user_controller from "../controllers/user.controllers.js";
+import { ensureValidId } from "../middlewares/validate.middleware.js";
+import { authMiddleware, requireRole } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+// Unprotected Routes (no requirements)
+router.post("/user", user_controller.create); // generates token
+
+router.post("/user/login", user_controller.login); // generates token
+
+// Protected Routes (requires JWT Token)
+router.get("/users/:id", authMiddleware(), ensureValidId, user_controller.get);
+
+router.put(
+  "/users/:id",
+  authMiddleware(),
+  ensureValidId,
+  user_controller.update
+);
+
+// Admin Routes (requires JWT Token + Admin Role)
+router.get(
+  "/users",
+  authMiddleware(),
+  requireRole("ADMIN"),
+  user_controller.list
+);
+
+router.delete(
+  "/users/:id",
+  authMiddleware(),
+  requireRole("ADMIN"),
+  ensureValidId,
+  user_controller.delete
+);
+
+export default router;
