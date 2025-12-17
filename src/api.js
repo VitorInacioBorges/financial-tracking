f06240c69001058453bf29fdb425081220125f9d
VitorInacioBorges <vitorinacioborges@gmail.com>
2025-12-17 12:13:59 -0300

    feat: added express configs in api.js file

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import express from "express";
+import userRoutes from "./routes/user.routes.js";
+import transactionRoutes from "./routes/transaction.routes.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+
+const app = express();
+
+app.use(express.json());
+app.use("api", userRoutes);
+app.use("api", transactionRoutes);
+app.user(errorMiddleware);
+
+export default app;
